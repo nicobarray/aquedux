811d517e8694497ca5044d32db786d4babd7c767
Nicolas Barray <nbarray@gmail.com>
2018-05-23 11:17:42 +0200

    todo: Update example

--- examples/todo/app/src/index.js
+++ examples/todo/app/src/index.js
@@ -1,25 +1,28 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, subscribeToChannel } from 'aquedux-client'
+import { applyMiddleware, compose } from 'redux'
+import { createStore, createAquedux, subscribe } from 'aquedux-client'
 import App from './components/App'
 import rootReducer from './reducers'
-import configureAquedux from './configureAquedux'
-
-//const store = createStore(rootReducer)
-const store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 const port = process.env.REACT_APP_AQUEDUX_PORT || '4242'
 const host = process.env.REACT_APP_AQUEDUX_HOST || 'localhost'
 const protocol = process.env.REACT_APP_AQUEDUX_PROTOCOL || 'http'
 
-const endpoint = `${protocol}://${host}:${port}/aquedux`
-const aquedux = configureAquedux(store, endpoint)
-aquedux.start()
+const aquedux = createAquedux({
+  hydratedActionTypes: ['ADD_TODO', 'TOGGLE_TODO'],
+  endpoint: `${protocol}://${host}:${port}/aquedux`,
+  channels: [ 'todos' ]
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(aquedux)))
+
 /**
  * In a real world app, this dispatch should be done in a container/component at route level
  */
-store.dispatch(subscribeToChannel('todos'))
+store.dispatch(subscribe('todos'))
 
 render(
   <Provider store={store}>
