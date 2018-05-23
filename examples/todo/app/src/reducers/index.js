1e65c2884eee57cfa767e6b4b373ff2d951e8826
Nicolas Barray <nbarray@gmail.com>
2018-05-24 00:48:32 +0200

    todo: remove useless aquedux reducer

--- examples/todo/app/src/reducers/index.js
+++ examples/todo/app/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
-import { aqueduxReducer } from 'aquedux-client'
 import todos from './todos'
 import visibilityFilter from './visibilityFilter'
 
 export default combineReducers({
   todos,
-  visibilityFilter,
-  aquedux: aqueduxReducer
+  visibilityFilter
 })
